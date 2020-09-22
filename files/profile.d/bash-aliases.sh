293fe3b5aead5ab5583d91d9b6e2fc4c3fdffcdf
Morten Lied Johansen <mortenjo@ifi.uio.no>
2020-09-22 10:56:08 +0200

    Try to handle SHELL not set when running dircolors

--- files/profile.d/bash-aliases.sh
+++ files/profile.d/bash-aliases.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
 # You may uncomment the following lines if you want `ls' to be colorized:
 
-if $(ls --color=auto &> /dev/null); then
-  export LS_OPTIONS='--color=auto'
-  eval "`dircolors`"
-fi
+function set_ls_options() {
+  local bash_path
+  bash_path="$(command -v bash)"
+  local dc_shell
+  dc_shell="${SHELL:-${bash_path}}"
+
+  if ls --color=auto &> /dev/null; then
+    export LS_OPTIONS='--color=auto'
+    eval "$(SHELL=${dc_shell} dircolors)"
+  fi
+}
+
+set_ls_options
 
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
